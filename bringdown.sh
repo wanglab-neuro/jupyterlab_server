b2c81a62b4c3b7d021395706dc1656a91ff365bf
Vincent <prevosto@mit.edu>
2021-12-01 18:50:58 -0500

    Update bringdown.sh

--- bringdown.sh
+++ bringdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
 docker-compose down
 docker volume rm jupyterhub_jupyterhub_data
-docker rmi jhub_ds
+docker rmi wanglabneuro/jhub_ds
 rm -rf /srv/jupyterhub/*
 
 # docker rm -v jupyterhub
