03a9b2fd85dbbfb083c57365c8957cea0d994757
Vincent Prevosto <prevosto@mit.edu>
2022-02-12 15:40:33 -0500

    updates post-spawn script for caiman container

--- jupyterlab/jlab_caiman/context/link_package_files.sh
+++ jupyterlab/jlab_caiman/context/link_package_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-WORK_DIRECTORY="/home/jovyan/work"
+WORK_DIRECTORY="/home/jovyan"
 
 # See if there are supporting files in the media folder, and link them to the home directory
 files=$(shopt -s nullglob dotglob; echo /media/*)
 if (( ${#files} )); then
   #Create target folder if needed
-  PACKAGE_DIRECTORY=$WORK_DIRECTORY/Package-Files
+  PACKAGE_DIRECTORY=$WORK_DIRECTORY/Packages
   if [ ! -d "$PACKAGE_DIRECTORY" ]; then
     mkdir -p $PACKAGE_DIRECTORY
   fi
@@ -20,12 +20,8 @@ else
   echo "Media directory is empty"
 fi
 
-# lastline="tail -1 $HOME/.bashrc"
+# if WORK_DIRECTORY is different from USER_DIRECTORY, make terminal start from there:
+# lastline="tail -1 $HOME/.profile"
 # if [ "$lastline" != "$WORK_DIRECTORY" ]; then
 #   echo "cd $HOME/work" >> ~/.bashrc
 # fi
-
-lastline="tail -1 $HOME/.profile"
-if [ "$lastline" != "$WORK_DIRECTORY" ]; then
-  echo "cd $HOME/work" >> ~/.bashrc
-fi
